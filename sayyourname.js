fbcbe412b98d78430535748fb9ee6f96c667a611
Dan Marshall <50140623+UntappedCode@users.noreply.github.com>
2019-09-26 09:15:37 -0400

    Add files via upload

---
+++ sayyourname.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+  var x = document.getElementById("myText").value;
+  var y = document.getElementById("myTextLastName").value;
+  document.getElementById("demo").innerHTML ="Hello " + x + " " + y;
+}
+
+
+
+
